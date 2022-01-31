ab8bdd684104cdfa535dfcac7807785aa0ee2f03
Baumbart13 <tkefer@tsn.at>
2022-01-31 10:17:01 +0100

    Introducing Observer Pattern

---
+++ 5/SWP/Web/Observer Pattern/src/main/java/htl/Main.java
@@ -0,0 +1,9 @@
+package htl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Observer Pattern\n" +
+                           "================\n\n");
+    }
+}
---
+++ 5/SWP/Web/Observer Pattern/src/main/java/htl/Zentrale.java
@@ -0,0 +1,4 @@
+package htl;
+
+public class Zentrale {
+}
